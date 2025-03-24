bc00b547ab9ef130df72aa8f5c95fc925fe2c004
matthew13dev <matthew.coder@outlook.com>
2025-03-24 05:28:05 -0300

    adiconado: Agenda 02 DS3, foi feito os metodos CRUD para a classe Produto

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
 
 namespace MauiAppMinhasCompras.Helpers
 {
     class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _connection;
+        public SQLiteDatabaseHelper(string dbPath)
+        {
+            _connection = new SQLiteAsyncConnection(dbPath);
+            _connection.CreateTableAsync<Produto>().Wait();
+        }
+
+
+        public Task<int> Insert(Produto produto)
+        {
+            return _connection.InsertAsync(produto);
+        }
+        public Task<List<Produto>> Update(Produto produto)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=? WHERE Id=?";
+            return _connection.QueryAsync<Produto>(sql, produto.Dedescricao, produto.Quantidade, produto.Preco, produto.Id);
+        }
+        public Task<int> Delete(int id)
+        {
+            return _connection.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> GetAll() 
+        { 
+            return _connection.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Seach(string palavra) 
+        {
+            string sql = "SELECT * Produto WHERE descricao LIKE '%" + palavra + "%'";
+            return _connection.QueryAsync<Produto>(sql);
+        }
     }
 }
